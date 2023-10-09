cf2438d91ef476ff14ad5e517ce20dd63dfe318a
txtFn <ennius33@gmail.com>
2023-10-10 07:53:10 +0800

    Major Design Changes

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const capitalAlphabet =
 const specialChar =
     ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', ',', '.', '?', '/']
 
+// Pass
 function randomizer() {
     var arr = []
     while (arr.length < 12) {
@@ -14,8 +15,12 @@ function randomizer() {
         arr.push(Math.floor(Math.random() * 10));
     }
     document.getElementById('pass').innerHTML = arr.join('');
+    document.getElementById('pass').style.color = '#186F65';
+    document.getElementById('pass').style.fontWeight = 'bold';
+    document.getElementById('pass').style.textShadow = '0px 0px 10px #B9FFF8';
 }
 
+// LongPass
 function longPassRandomizer() {
     var arr = []
     while (arr.length < 20) {
@@ -25,20 +30,41 @@ function longPassRandomizer() {
         arr.push(Math.floor(Math.random() * 10));
     }
     document.getElementById('longPass').innerHTML = arr.join('');
+    document.getElementById('longPass').style.color = '#186F65';
+    document.getElementById('longPass').style.fontWeight = 'bold';
+    document.getElementById('longPass').style.textShadow = '0px 0px 10px #B9FFF8';
 }
 
+// SixPin
 function sixPinRandomizer() {
     var arr = []
     while (arr.length < 6) {
         arr.push(Math.floor(Math.random() * 10));
     }
     document.getElementById('sixPin').innerHTML = arr.join('');
+    document.getElementById('sixPin').style.color = '#186F65';
+    document.getElementById('sixPin').style.fontWeight = 'bold';
+    document.getElementById('sixPin').style.textShadow = '0px 0px 10px #B9FFF8';
 }
 
+// FourPin
 function fourPinRandomizer() {
     var arr = []
     while (arr.length < 4) {
         arr.push(Math.floor(Math.random() * 10));
     }
     document.getElementById('fourPin').innerHTML = arr.join('');
+    document.getElementById('fourPin').style.color = '#186F65';
+    document.getElementById('fourPin').style.fontWeight = 'bold';
+    document.getElementById('fourPin').style.textShadow = '0px 0px 10px #B9FFF8';
 }
+
+// Sidenav
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+// MaterialBox
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+});
