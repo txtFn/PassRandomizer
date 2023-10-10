d90f76d69bd9a31134ec564ceec1b9bab873bc0e
txtFn <ennius33@gmail.com>
2023-10-10 16:40:18 +0800

    Bold Title and Refine javascript code for password generator

--- main.js
+++ main.js
@@ -1,38 +1,52 @@
 const smallAlphabet =
-    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const capitalAlphabet =
-    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const specialChar =
-    ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', ',', '.', '?', '/']
+    ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', ',', '.', '?', '/'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// Pass
+const strongPass = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', ',', '.', '?', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// PassGenerator
+function generatePass(a, b) {
+    while (a.length < b) {
+        a.push(strongPass[Math.floor(Math.random() * 89)]);
+    }
+}
+
+// ShortPass
 function randomizer() {
     var arr = []
-    while (arr.length < 12) {
-        arr.push(smallAlphabet[Math.floor(Math.random() * 26)]);
-        arr.push(capitalAlphabet[Math.floor(Math.random() * 26)]);
-        arr.push(specialChar[Math.floor(Math.random() * 27)]);
-        arr.push(Math.floor(Math.random() * 10));
+    var length = 12;
+    generatePass(arr, length); // first to execute
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] in specialChar || arr[i] in capitalAlphabet || arr[i] in specialChar || arr[i] in numbers) {
+            document.getElementById('pass').innerHTML = arr.join('');
+            document.getElementById('pass').style.color = '#186F65';
+            document.getElementById('pass').style.fontWeight = 'bold';
+            document.getElementById('pass').style.textShadow = '0px 0px 10px #B9FFF8';
+        } else {
+            randomizer();
+        }
     }
-    document.getElementById('pass').innerHTML = arr.join('');
-    document.getElementById('pass').style.color = '#186F65';
-    document.getElementById('pass').style.fontWeight = 'bold';
-    document.getElementById('pass').style.textShadow = '0px 0px 10px #B9FFF8';
 }
 
 // LongPass
 function longPassRandomizer() {
     var arr = []
-    while (arr.length < 20) {
-        arr.push(smallAlphabet[Math.floor(Math.random() * 26)]);
-        arr.push(capitalAlphabet[Math.floor(Math.random() * 26)]);
-        arr.push(specialChar[Math.floor(Math.random() * 27)]);
-        arr.push(Math.floor(Math.random() * 10));
+    var length = 20
+    generatePass(arr, length); // first to execute
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] in specialChar || arr[i] in capitalAlphabet || arr[i] in specialChar || arr[i] in numbers) {
+            document.getElementById('longPass').innerHTML = arr.join('');
+            document.getElementById('longPass').style.color = '#186F65';
+            document.getElementById('longPass').style.fontWeight = 'bold';
+            document.getElementById('longPass').style.textShadow = '0px 0px 10px #B9FFF8';
+        } else {
+            longPassRandomizer();
+        }
     }
-    document.getElementById('longPass').innerHTML = arr.join('');
-    document.getElementById('longPass').style.color = '#186F65';
-    document.getElementById('longPass').style.fontWeight = 'bold';
-    document.getElementById('longPass').style.textShadow = '0px 0px 10px #B9FFF8';
 }
 
 // SixPin
